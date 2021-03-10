097e7fd815c5adb1956a61f91d0fee1e8cf37eaa
Harsh <hjscrt@gmail.com>
2021-03-09 17:51:42 -0800

    lab_week_9

---
+++ APP.js
@@ -0,0 +1,17 @@
+const testnumber1 = 1;
+const testnumber2 = 3;
+
+let testnumber3 = "Harsh";
+let testnumber4 = true;
+
+if (testnumber1 === testnumber2) 
+{
+    console.log('they match');
+} else {
+    console.log('no luck');
+}
+
+for (x of testnumber3) 
+{
+    console.log(x);
+} 
